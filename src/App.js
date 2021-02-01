b5d773f63dfc7e61ad56e84e70b628fba3ce5378
Vikaramjit Singh Goraya <vikaramjitsinghgorraya@gmail.com>
2021-01-31 21:18:57 -0700

    Uploading app.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Card from './components/Card'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
